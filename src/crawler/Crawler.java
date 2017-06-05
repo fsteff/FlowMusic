cdb4e670566560f365e24ca49732debb24c566a4
Vanilli <Vanilli@>
2017-06-05 11:46:47 +0200

    gets title or artist from file name

--- src/crawler/Crawler.java
+++ src/crawler/Crawler.java
@@ -14,14 +14,29 @@ import central.Component;
 import central.ThreadedComponent;
 import crawler.file.MP3File;
 
+/**
+ * The crawler searches through a given file path for mp3 files
+ * and sends them to the database
+ *  
+ *  @author Vanessa Fediuk
+ */
 public class Crawler extends ThreadedComponent {
 
 	private static final Logger logger = LoggerFactory.getLogger(Crawler.class);
+	private JSONArray mp3Files = new JSONArray();
+	private String title="";
+	private String artist="";
 	
 	public Crawler(Central central) {
 		super(Component.CRAWLER, central);
 	}
+	
 
+	/**
+	 * gets all paths of the files in the given directory
+	 * 
+	 * @ @param file
+	 */
 	public String[] getPaths(File file) {
 		// gets all files in the given file
 		File[] files = file.listFiles();
@@ -37,6 +52,12 @@ public class Crawler extends ThreadedComponent {
 		}
 	}
 
+	/**
+	* searches in the given paths for mp3Files
+	* and adds them to a JSONArray
+	* 
+	* @param mp3Files,paths
+	*/
 	public void searchForMp3(JSONArray mp3Files, String[] paths) {
 		
 		if (paths != null) {
@@ -52,22 +73,77 @@ public class Crawler extends ThreadedComponent {
 					// already found ones if it is
 					try {
 						MP3File mp3 = new MP3File(path);
-
+						//if the data in the mp3 file for title or artist is empty 
+						//get it form the file name
+						title=mp3.getTitle();
+						artist=mp3.getArtist();
+						testForNullValues(file);
+						
+						//saves the found data in a JSONObject to put it in the JSONArray
 						JSONObject data = new JSONObject();
-						data.put("title", mp3.getTitle());
-						data.put("artist", mp3.getArtist());
+						data.put("title", title);
+						data.put("artist", artist);
 						data.put("album", mp3.getAlbum());
 						data.put("path", path);
 						mp3Files.put(data);
 
 					} catch (Exception e) {
-						// do nothing
+						//if the file has no tag get the data for title and artist from the file name 
+						title="";
+						artist="";
+						testForNullValues(file);
+						
+						//saves the existing data in s JSONObject to put it in the JSONArray
+						JSONObject data = new JSONObject();
+						data.put("title", title);
+						data.put("artist", artist);
+						data.put("path", path);
+						mp3Files.put(data);
 					}
 				}
 			}
 		}
 	}
 
+	/**
+	 * gets the title or the artist from the file name
+	 * 
+	 * @param file
+	 */
+	public void testForNullValues(File file){
+		int i=0;
+		//gets the title from the file name if it was empty
+		if(title.equals("")){
+			StringBuffer sb1=new StringBuffer();
+			//if the file name contains a '-' we assume that the second part is the title
+			if(file.getName().contains("-")){
+				i=file.getName().indexOf('-')+1;
+				while(!file.getName().substring(i, i+4).equalsIgnoreCase(".mp3")){
+					sb1.append(file.getName().charAt(i));
+					i++;
+				}
+			//if the file name has no special form the full name is the title
+			}else{
+				while(!file.getName().substring(i, i+4).equalsIgnoreCase(".mp3")){
+					sb1.append(file.getName().charAt(i));
+					i++;
+				}		
+			}
+			title=sb1.toString();
+		}
+		//gets the artist form the file name if it was empty
+		//if the file name contains a '-' we assume that the first part is the artist
+		if(artist.equals("")&&file.getName().contains("-")){
+			i=0;
+			StringBuffer sb2=new StringBuffer();
+			while(file.getName().charAt(i)!='-'){
+				sb2.append(file.getName().charAt(i));
+				i++;
+			}
+			artist=sb2.toString();
+		}
+	}
+	
 	public void shutdown() {
 	}
 
@@ -76,13 +152,15 @@ public class Crawler extends ThreadedComponent {
 		if (sender == Component.CENTRAL) {
 			String command = msg.getString("command");
 			if (command.equalsIgnoreCase("start")) {
-				// gets the paths that should be searched
+				// gets the paths and searches them
 				sendMessage(Component.CENTRAL, Central.Messages.getConfig(), this.onNewConfig);
 				return new JSONObject("{\"answer\":\"started\"}");
 
 			} else if (command.equalsIgnoreCase("config changed")) {
+				//searches for mp3files if the to search paths have changed
 				this.onNewConfig.accept(msg);
 			} else if (command.equalsIgnoreCase("shutdown")) {
+				//shuts the crawler down
 				shutdown();
 				return new JSONObject("{\"answer\":\"done\"}");
 			}
@@ -90,14 +168,17 @@ public class Crawler extends ThreadedComponent {
 		return null;
 	}
 
+	/**
+	 * gets the paths to searches them for mp3 files and updates the database
+	 */
 	private Consumer<JSONObject> onNewConfig = jsonObject -> {
+		//gets the paths that should be searched
 		JSONObject cfg = jsonObject.getJSONObject(Central.Messages.CONFIG);
 		JSONArray paths = cfg.optJSONArray(Central.Config.MUSIC_DIRS);
 		String[] params = new String[paths.length()];
 		for (int i = 0; i < paths.length(); i++) {
 			params[i] = paths.getString(i);
 		}
-        JSONArray mp3Files = new JSONArray();
 		searchForMp3(mp3Files,params);
 		JSONArray found = mp3Files;
 
